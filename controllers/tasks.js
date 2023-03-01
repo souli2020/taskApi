baa2bdbdcde0cea8a8fc5080dcf145db108a2b62
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-02 00:05:54 +0100

    Catch errors in the middleware

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,12 +5,18 @@ const noTry = require('../middlewares/noTry')
 
 const getAllTasks = async (req, res) => {
     const tasks = await Task.find({})
+    if (tasks.length < 1) {
+        return res.status(404).json({ msg: "No tasks" })
+    }
     res.status(200).json({ tasks })
 }
 
 const getSingleTask = async (req, res) => {
     const { id } = req.params
     const task = await Task.findOne({ _id: id })
+    if (!task) {
+        return res.status(404).json(`no task found with id : ${id}`)
+    }
     res.status(200).json({ task })
 }
 
@@ -22,18 +28,25 @@ const createTask = async (req, res) => {
 const deleteTask = async (req, res) => {
     const { id } = req.params
     const task = await Task.findOneAndDelete({ _id: id })
+    if (!task) {
+        return res.status(404).json(`no task found with id : ${id}`)
+    }
     res.status(200).json({ task })
 }
 
 const updateTask = async (req, res) => {
     const { id } = req.params
     const task = await Task.findOneAndUpdate({ _id: id }, req.body, { new: true, runValidators, upsert: true })
+    if (!task) {
+        return res.status(404).json(`no task found with id : ${id}`)
+    }
     res.status(200).json({ task })
 }
 
 const deleteAllTasks = async (req, res) => {
     const task = await Task.deleteMany()
-    res.status(200).json({ task })
+
+    res.status(200).json({ msg: "All tasks deleted" })
 }
 
 module.exports = {
--- middlewares/noTry.js
+++ middlewares/noTry.js
@@ -1,7 +1,10 @@
-const tryWrapper = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
-    };
-};
+const tryWrapper = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+
+        res.status(500).json({ msg: error })
+    }
+}
 
 module.exports = tryWrapper
